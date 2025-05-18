34e7c89a381b97e8baf452a4fa26d43534ef76d1
bilimig <bilimig@wp.pl>
2025-05-18 16:37:23 +0200

    added make-change, revert,  sorted-items to solution instance

---
+++ build/CMakeFiles/KnapsackProblemSolver.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KnapsackProblemSolver.dir/src/knapsack_problem_instance.cpp.o"
+  "CMakeFiles/KnapsackProblemSolver.dir/src/knapsack_problem_instance.cpp.o.d"
+  "CMakeFiles/KnapsackProblemSolver.dir/src/main.cpp.o"
+  "CMakeFiles/KnapsackProblemSolver.dir/src/main.cpp.o.d"
+  "KnapsackProblemSolver"
+  "KnapsackProblemSolver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KnapsackProblemSolver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/KnapsackProblemSolver.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for KnapsackProblemSolver.
+# This may be replaced when dependencies are built.
